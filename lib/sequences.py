b2c823beee7643c74bb2e43193537eba4f95d12a
macray <macraykorir254@gmail.com>
2023-08-21 09:09:15 +0300

    Completed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 
-def print_fibonacci(length):
-    pass
+def test_print_fibonacci_zero(self):
+    '''prints empty list when length = 0'''
+    captured_out = io.StringIO()
+    sys.stdout = captured_out
+    print_fibonacci(0)
+    sys.stdout = sys.__stdout__
+    assert captured_out.getvalue().strip() == '[]'
